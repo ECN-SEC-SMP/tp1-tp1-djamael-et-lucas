bdace8b6f8865eb9d9478b15e150f900b3a32ef2
Lucas_Oros <oroslucas1604@gmail.com>
2025-10-06 08:23:42 +0200

    Init

---
+++ TP1-Lucas/lexique.cpp
@@ -0,0 +1,11 @@
+//#include <stdio.h>
+#include <iostream>
+#include "lexique.h"
+#include <math.h>  
+#include <fstream> 
+#include <map>
+#include <sstream>
+
+
+using namespace std;
+
---
+++ TP1-Lucas/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * LUCAS OROS 
+ * TP1 - SEC - 06/10/2025
+ * CPP - TP1
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include "lexique.h"
+
+using namespace std;
+
+
+int main()
+{
+    
+}
+
